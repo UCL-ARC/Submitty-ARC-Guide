386a1995ce36770ff7aa949d0fd3d1587135bd9a
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2021-12-23 22:26:32 +0000

    Adds the information and script to grab the grades

--- docs/instructor/index.rst
+++ docs/instructor/index.rst
@@ -1,3 +1,66 @@
 ================
 Instructor Guide
 ================
+
+Requests and Create Course
+==========================
+
+
+Create assignment
+=================
+
+
+Grade assignments
+=================
+
+
+Release grades
+==============
+
+Once the automated and manual grading has been completed in Submitty, we can
+proceed to release the grades. We need to extract the marks from submitty,
+combine the manual and automated grades according to your needs, generate the
+feedback reports and combine them with Moodle's grade list.
+
+Collect marks from Submitty
+---------------------------
+
+To be able to collect the combine marks from Submitty, first we need to generate
+`Grades summary`_. This is done by clicking "Generate Grade Summaries" under the
+"Grade Reports" page in the left bar menu for the course you want to collect the
+marks from. This button doesn't generate anything "visible", it only will tell
+you when was the last time run. In the background, it generates a file for each
+submission that we will use to combine marks and generate feedback reports.
+
+.. note::
+   If you only need to download the numeric final mark, then generating the
+   downloadable CSV Report will provide that for each of the assignments. This
+   won't export the feedback or break down of the marks.
+
+Next step is to collect all the details for each gradeable. For that we need to run
+a script on the Submitty machine. SSH into it and run ``grades_extractor``.
+
+.. code-block:: bash
+
+   submitty$ grades_extractor -c <coursename> -a <assignment>
+
+
+This generates a ``results_<assignment>.tar.bz2`` file. Download that file locally to
+process it.
+
+.. code-block:: bash
+
+   local$ rsync -azvh submitty:~/results_<assignments>.tar.gz .
+   local$ tar jxvf results_<assignment>.tar.bz2
+
+
+Now in your local computer you'd have a directory ``results_<assignment>`` containing
+directories for each submission (with the student id), and each containing two files:
+``<studentId>_automated.json`` and ``<studentId>_manual.json``.
+
+
+
+
+
+
+.. _Grades summary: https://submitty.org/instructor/course_settings/rainbow_grades/#grades-summaries
---
+++ submitty_arc/instructor/grades_extractor.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from argparse import ArgumentParser
+import os
+import getpass
+import shutil
+
+def list_students(instructor="ucasper", course="mphy0021test", assignment="alchemist"):
+    submissions = [p.name for p in Path(f'/home/{instructor}/{course}/submissions/{assignment}/').iterdir()]
+    return list(filter(lambda x: instructor not in x, submissions))
+
+
+def copy_grades(student_id, outdir, instructor="ucasper", course="mphy0021test", assignment="alchemist"):
+    basepath = f'/home/{instructor}/{course}/results/{assignment}/{student_id}/'
+    directories = [p.name for p in Path(basepath).iterdir()]
+    versions = []
+    for name in directories:
+        try:
+            versions.append(int(name))
+        except ValueError:
+            pass
+    submission_dir = Path(basepath) / f'{max(versions)}'
+    autograde = submission_dir / 'results.json'
+
+    manual_grade = Path(f'/home/{instructor}/{course}/reports/all_grades/{student_id}_summary.json')
+
+
+    destination_dir = outdir / student_id
+    destination_dir.mkdir()
+
+    shutil.copy(autograde, destination_dir / f'{student_id}_automated.json')
+    shutil.copy(manual_grade, destination_dir / f'{student_id}_manual.json')
+
+
+def run_it():
+    parser = ArgumentParser(description="Extracts the files for manual and automatic grading.")
+    parser.add_argument('--course', '-c', default='mphy0021', help='Course name as the directory in your home')
+    parser.add_argument('--assignment', '-a', default='gamelife', help='assignment you want to collect')
+    parser.add_argument('--instructor', '-i', default=getpass.getuser(), help='Instructor user')
+    parser.add_argument('--outdir', '-o', default='.', help='where the output directory will be created, it will create a "results_{assignment}" directory')
+    args = parser.parse_args()
+
+    outdir = Path(args.outdir)
+    outpath = outdir / f"results_{args.assignment}"
+    outpath.mkdir(parents=True)
+
+    students = list_students(args.instructor, args.course, args.assignment)
+    for student in students:
+        copy_grades(student, outpath, instructor=args.instructor, course=args.course, assignment=args.assignment)
+
+    shutil.make_archive(f"{outpath.name}", 'bztar', outdir, outpath.name)
+
+if __name__ == "__main__":
+    run_it()
